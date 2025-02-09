f5b034777d1c18c2da6812374baa26025d003ebe
Proctored Games <proctoredgames@hotmail.com>
2025-02-09 15:45:11 -0800

    Initial commit

---
+++ src/main/java/net/proctoredgames/nationsmod/NationsModClient.java
@@ -0,0 +1,10 @@
+package net.proctoredgames.nationsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NationsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
