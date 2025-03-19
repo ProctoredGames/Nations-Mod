5016f31ae1cbceaac2fc86ea2408be55d213f14e
Proctored Games <proctoredgames@hotmail.com>
2025-03-19 15:58:18 -0700

    fae villager renderer registered

--- src/main/java/net/proctoredgames/nationsmod/NationsModClient.java
+++ src/main/java/net/proctoredgames/nationsmod/NationsModClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.EntityType;
+import net.proctoredgames.nationsmod.entity.client.villagers.Fae;
 import net.proctoredgames.nationsmod.entity.client.villagers.Nation3Villager;
 //import net.proctoredgames.nationsmod.entity.client.Nation2TridentEntityRenderer;
 //import net.proctoredgames.nationsmod.entity.client.Nation4TridentEntityRenderer;
@@ -14,6 +15,7 @@ import net.proctoredgames.nationsmod.entity.client.villagers.Nation3Villager;
 public class NationsModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        EntityModelLayerRegistry.registerModelLayer(Fae.FAE, Fae::getTexturedModelData);
         EntityModelLayerRegistry.registerModelLayer(Nation3Villager.NATION_3_VILLAGER, Nation3Villager::getTexturedModelData);
     }
 }
