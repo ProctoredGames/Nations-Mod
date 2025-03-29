058bb9fda0c70a678b10bae6ee9c1fe4b3511f07
Proctored Games <proctoredgames@hotmail.com>
2025-03-29 15:51:22 -0700

    fixed recipes

--- src/main/java/net/proctoredgames/adventurecraft/datagen/ModItemTagProvider.java
+++ src/main/java/net/proctoredgames/adventurecraft/datagen/ModItemTagProvider.java
@@ -17,7 +17,18 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-
+        getOrCreateTagBuilder(ItemTags.TRIM_MATERIALS)
+                .add(ModItems.ESSENCE_OF_EMPIRE_1)
+                .add(ModItems.ESSENCE_OF_EMPIRE_2)
+                .add(ModItems.ESSENCE_OF_EMPIRE_3)
+                .add(ModItems.ESSENCE_OF_EMPIRE_4)
+                .add(ModItems.ESSENCE_OF_EMPIRE_5)
+                .add(ModItems.ESSENCE_OF_EMPIRE_6)
+                .add(ModItems.ESSENCE_OF_EMPIRE_7)
+                .add(ModItems.ESSENCE_OF_EMPIRE_8)
+                .add(ModItems.ESSENCE_OF_EMPIRE_9);
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+                .add(Items.ELYTRA);
 
     }
 }
